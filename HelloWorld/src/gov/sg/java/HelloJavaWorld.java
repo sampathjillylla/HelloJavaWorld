9ecb07423adc86ffeae4727ee10832ce278993f5
USER <USER@USER-PC>
2020-05-07 04:55:19 +0800

    First Commit

---
+++ HelloWorld/src/gov/sg/java/HelloJavaWorld.java
@@ -0,0 +1,10 @@
+package gov.sg.java;
+
+public class HelloJavaWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Hello Java World :: This is My First Git and Eclipse Project");
+	}
+
+}
